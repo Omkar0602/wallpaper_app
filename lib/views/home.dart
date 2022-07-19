7503f9bb8ee0da9340af6f458b8e704c132c96e0
Omkar0602 <omkarkhandebharad@gmail.com>
2022-07-19 20:10:15 +0530

    categorie wallpapers

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,16 @@
+
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 import 'package:wallpaper_app/data/data.dart';
-import 'package:wallpaper_app/views/categories.dart';
+import 'package:wallpaper_app/model/categories.dart';
+import 'package:wallpaper_app/model/wallpaper_model.dart';
+import 'package:wallpaper_app/views/categoriescreen.dart';
+import 'package:wallpaper_app/views/search.dart';
 import 'package:wallpaper_app/views/widget/widget.dart';
+import 'package:http/http.dart' as http;
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -11,11 +19,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+ 
   @override
   List<CategoriesModel> categories = [];
+  List wallpapers =[];
+  
+  int page=1;
+
+  getTrendingWallpaper()async{
+      await http.get(Uri.parse(  "https://api.pexels.com/v1/curated?per_page=80"),
+    headers:{
+      "Authorization": apiKey
+      },
+      ).then((value) {
+        Map result =jsonDecode(value.body);
+        setState(() {
+      wallpapers=result['photos'];
+    });
+      });
+    
+    
+  }
+  loadmore()async{
+    setState(() {
+      page =page +1;
+    });
+    String url='https://api.pexels.com/v1/curated?per_page=80&page='+page.toString();
+    await http.get(Uri.parse(url),
+    headers:{
+      "Authorization": apiKey
+      },
+      ).then((value){
+        Map result =jsonDecode(value.body);
+        setState(() {
+      wallpapers.addAll(result['photos']) ;
+    });
+      });
+
+  }
+    ScrollController _scrollController = new ScrollController();
+    TextEditingController _query =new TextEditingController();
+
   void initState() {
+    getTrendingWallpaper();
     categories = getCategories();
     super.initState();
+    
+    
   }
   @override
   Widget build(BuildContext context) {
@@ -24,7 +74,9 @@ class _HomeState extends State<Home> {
                   appBar: AppBar(title: BrandName(),
                   backgroundColor: Colors.white,
                   centerTitle: true,
-                  elevation: 0.0),
+                  elevation: 0.0,
+                  ),
+                  
 
                   body: Container(
 
@@ -46,6 +98,7 @@ class _HomeState extends State<Home> {
                             children: [
                               Expanded(
                                 child: TextField(
+                                  controller: _query,
                                   decoration: InputDecoration(
                                     hintText: "Search Wallpaper",
                                     border: InputBorder.none,
@@ -53,7 +106,11 @@ class _HomeState extends State<Home> {
                                   ),
                                 ),
                               ),
-                              Icon(Icons.search),
+                              GestureDetector(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(builder: ((context) => Search(query: _query.text))));
+                                },
+                                child: Icon(Icons.search)),
                             ],
                           ),
                         ),
@@ -62,7 +119,7 @@ class _HomeState extends State<Home> {
 
                       
                      Container(
-                      height: 80,
+                      height: 60,
                       
                        child: ListView.builder(
                         padding: EdgeInsets.symmetric(horizontal: 20),
@@ -72,7 +129,40 @@ class _HomeState extends State<Home> {
                             return CategoriesTitle(title: categories[index].categoriesName, imgUrl:categories[index].imgUrl );
                           },),
                      ),
-                      
+                     SizedBox(height:20 ,),
+                     Expanded(
+                       child: Container(
+                         padding: EdgeInsets.symmetric(horizontal: 16),
+                         decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                         child: NotificationListener<ScrollNotification>(
+
+                          onNotification: (scrollNotification){
+                            if(scrollNotification is ScrollEndNotification){
+                              loadmore();
+                              
+                            }
+                            return true;
+                          },
+                           child: GridView.builder(
+                             shrinkWrap: true,
+                             itemCount: wallpapers.length,
+                           
+                           itemBuilder: (BuildContext context, int index) {return Container(
+                             child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(wallpapers[index]['src']['large2x'],fit: BoxFit.cover,)),
+                           );  },
+                           gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                                                 crossAxisSpacing: 6,
+                                                 crossAxisCount: 2,
+                                                 childAspectRatio: 2 / 3,
+                                                 mainAxisSpacing: 6),
+                           
+                           ),
+                         ),
+                       ),
+                     ),
+                     
                     ],
                   ),
                 ),
@@ -88,41 +178,46 @@ class CategoriesTitle extends StatelessWidget {
   @override
 
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 150,
-      padding: EdgeInsets.only(left: 10),
-      child: Stack(
-        children: [
-          Container(
-            
-            
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>CategorieWallpaper(categorie: title)));
+      },
+      child: Container(
+        height: 60,
+        width: 150,
+        padding: EdgeInsets.only(left: 10),
+        child: Stack(
+          children: [
+            Container(
               
-              image: DecorationImage(
-                fit: BoxFit.cover,
+              
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
                 
-                image: NetworkImage(imgUrl,))
-            ),
-           
-          ),
-           Container(
-              height: 80,
-      width: 150,
-      decoration: BoxDecoration(
-        color: Colors.black26,
-              borderRadius: BorderRadius.circular(10),
-              
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  
+                  image: NetworkImage(imgUrl,))
+              ),
              
             ),
-      
-      alignment: Alignment.center,
-              
-              child: Text(title,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),),
-            ),
-          
-        ],
+             Container(
+                height: 60,
+        width: 150,
+        decoration: BoxDecoration(
+          color: Colors.black26,
+                borderRadius: BorderRadius.circular(10),
+                
+               
+              ),
+        
+        alignment: Alignment.center,
+                
+                child: Text(title,style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),),
+              ),
+            
+          ],
+        ),
       ),
     );
   }
